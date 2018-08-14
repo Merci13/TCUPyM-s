cb683fbbfed6f058767226b7cb5f1f506890c8db
Merci13 <mrjorxe@gmail.com>
2018-08-13 23:27:54 -0600

    actualizacion de imagenes
    se agregaron imagenes y logos

--- Materialize/js/script.js
+++ Materialize/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.datepicker').datepicker();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true
+    });
+    $('.materialboxed').materialbox();
+    
   });
+  
+ 
